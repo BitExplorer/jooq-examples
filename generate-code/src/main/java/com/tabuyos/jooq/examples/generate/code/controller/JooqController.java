6964ba284a16aefc75222729f4f1b516ee7cc26b
Tabuyos <tabuyos@outlook.com>
2021-03-07 22:42:24 +0800

    modified JooqController.java.

--- generate-code/src/main/java/com/tabuyos/jooq/examples/generate/code/controller/JooqController.java
+++ generate-code/src/main/java/com/tabuyos/jooq/examples/generate/code/controller/JooqController.java
@@ -1,8 +1,10 @@
 package com.tabuyos.jooq.examples.generate.code.controller;
 
 import com.tabuyos.jooq.examples.generate.code.model.tables.User;
+import com.tabuyos.jooq.examples.generate.code.model.tables.daos.UserDao;
+import com.tabuyos.jooq.examples.generate.code.model.tables.records.UserRecord;
 import org.jooq.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.jooq.impl.DSL;
 import org.springframework.stereotype.Component;
 
 import static com.tabuyos.jooq.examples.generate.code.model.Tables.*;
@@ -14,15 +16,29 @@ import static org.jooq.impl.DSL.countDistinct;
 @Component
 public class JooqController {
   private final DSLContext create;
+  private final UserDao userDao;
 
-  public JooqController(DSLContext create) {
+  public JooqController(DSLContext create, UserDao userDao) {
     this.create = create;
+    this.userDao = userDao;
   }
 
   public void init() {
-    User user = USER.as("u");
-    Result<Record1<String>> fetch = create.select(user.NAME).from(user).fetch();
-    SelectJoinStep<Record> from = create.select().from(USER);
-    System.out.println(from.getSQL());
+    Result<UserRecord> fetch = create.selectFrom(USER).fetch();
+    UserRecord userRecord = create.selectFrom(USER).fetchAny();
+    Result<Record> fetch1 = create.select().from(USER).fetch();
+    Record record = create.select().from(USER).fetchAny();
+    assert record != null;
+    String name = record.get(USER.NAME, String.class);
+    create.select().from(USER).fetch();
+    Result<Record> fetch2 = create.select().from(USER).fetch();
+    ((UserRecord) fetch2).getId();
+    User c = new User();
+    create.insertInto(c).values().execute();
+    userDao.update();
+    userDao.delete();
+    userDao.insert();
+    userDao.findById(1);
+    userDao.findAll();
   }
 }
